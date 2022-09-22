4aca5ce2b023a0e179fb7f720ee65d3569bd687f
Emad <m.akhavan.saleh@gmail.com>
2022-09-22 09:32:59 +0330

    add controller & view to IdentityServer Project

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,5 +1,9 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
+using IdentityServer4.Validation;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -10,22 +14,63 @@ namespace IdentityServer
     public class Config
     {
         public static IEnumerable<Client> Clients => new Client[]{
-        new Client // قدم دوم تنظیمات
-            {
-                ClientId = "movieClient",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets =
+            new Client // قدم دوم تنظیمات
                 {
-                    new Secret("secret".Sha256())
+                    ClientId = "movieClient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "movieAPI" }
                 },
-                AllowedScopes = { "movieAPI" }
-            }
+            new Client
+                {
+                    ClientId = "movies_mvc_client",
+                    ClientName = "Movies MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signin-oidc" // this is client app port
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes=new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
         };
         public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]{
             new ApiScope("movieAPI","Movie API") // قدم اول تنظیمات
         };
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
-        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
-        public static List<TestUser> TestUsers => new List<TestUser> { };
+        public static IEnumerable<IdentityResource> IdentityResources => 
+            new IdentityResource[] { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        public static List<TestUser> TestUsers => 
+            new List<TestUser> { 
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username =  "mehmet",
+                    Password = "mehmet",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName,"mehmet"),
+                        new Claim(JwtClaimTypes.FamilyName,"ozkaya"),
+                    }
+                }
+            };
     }
 }
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -4,20 +4,35 @@ using IdentityServer4.Test;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews(); // اضافه کردن سرویس مربوط به کنترلر + نما
+
 //builder.Services.AddIdentityServer();   // اضافه کردن به لیست سرویس ها
 
 builder.Services.AddIdentityServer() // اضافه کردن به لیست سرویس به همراه انجام تنظیمات لازم
     .AddInMemoryClients(Config.Clients)
-    //.AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
     //.AddInMemoryApiResources(Config.ApiResources)
     .AddInMemoryApiScopes(Config.ApiScopes)
-    //.AddTestUsers(Config.TestUsers)
+    .AddTestUsers(Config.TestUsers)
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
+app.UseStaticFiles(); // فایل های استاتیک
+app.UseRouting(); // مسیریابی
 app.UseIdentityServer(); // استفاده از سرویس در برنامه
+app.UseAuthorization(); // انجام عملیات احراز هویت
 
-app.MapGet("/", () => "Hello World!");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute(); // آدرس دهی پیش فرض مربوط به کنترلرها
+
+    //endpoints.MapGet("/", async context =>
+    //{
+    //    await context.Response.WriteAsync("Hello World!");
+    //});
+});
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
