1d18b46f184baa06641c8a51da7decd3022c94b1
Emad <m.akhavan.saleh@gmail.com>
2022-09-22 18:22:59 +0330

    add api link

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -1,4 +1,6 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
+using Newtonsoft.Json;
 
 namespace Movies.Client.ApiServices
 {
@@ -21,6 +23,64 @@ namespace Movies.Client.ApiServices
 
         public async Task<IEnumerable<Movie>> GetMovies()
         {
+            // 1 - Get Token from Identity Server, of course we should provide the IS configurations like address client and secret.
+            // 2 - Send request to protected API
+            // 3 - Deserialize Object to MovieList
+
+
+            // 1. "retrieve" our api credentials. this must be registered on Identity Server!
+            var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:7235/connect/token",
+
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+
+                // this is the scope our Protected API requires.
+                Scope = "movieAPI"
+            };
+
+
+            // create a new HttpClient to talk to our IdentityServer (localhost:7235)
+            var client = new HttpClient();
+
+
+            // just checks if we can reach the discovery document. Not 100% needed but...
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:7235");
+            if (disco.IsError)
+            {
+                return null; // throw 500 error
+            }
+
+
+            // 2. Authenticates and get an access token from Identity Server
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+
+            // 2 - Send request to protected API
+
+            // Another HttpClient for talking now with our Protected API
+            var apiClient = new HttpClient();
+
+            // 3. Set the access_token in the request Authorization: Bearer <token>
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            // 4. Send a request to our Protected API
+            var response = await apiClient.GetAsync("https://localhost:7280/api/movie");
+            response.EnsureSuccessStatusCode();
+
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Deserialize Object to MovieList 
+            List<Movie>? movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return await Task.FromResult(movieList);
+
+            /* روش قدیم
             var movieList = new List<Movie>();
             movieList.Add(new Movie
             {
@@ -34,6 +94,7 @@ namespace Movies.Client.ApiServices
             });
 
             return await Task.FromResult(movieList);
+            */
         }
 
         public Task<Movie> UpdateMovie(Movie movie)
